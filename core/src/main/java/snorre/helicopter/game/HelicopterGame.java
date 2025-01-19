e5ba89ed7832cb5ededfa60cb91d168e16680b49
Snorre Sæther <snosaet@gmail.com>
2025-01-19 02:03:08 +0100

    adds input

--- core/src/main/java/snorre/helicopter/game/HelicopterGame.java
+++ core/src/main/java/snorre/helicopter/game/HelicopterGame.java
@@ -2,10 +2,12 @@ package snorre.helicopter.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -47,6 +49,7 @@ public class HelicopterGame extends ApplicationAdapter {
     @Override
     public void render() {
         draw();
+        input();
         logic();
     }
 
@@ -80,43 +83,79 @@ public class HelicopterGame extends ApplicationAdapter {
         return min + r.nextFloat() * (max - min);
     }
 
-    private void logic(){
+    private boolean isControlled(){
+        return Gdx.input.isKeyPressed(Input.Keys.RIGHT)
+            || Gdx.input.isKeyPressed(Input.Keys.LEFT)
+            || Gdx.input.isKeyPressed(Input.Keys.UP)
+            || Gdx.input.isKeyPressed(Input.Keys.DOWN);
+    }
+
+    private void input(){
+        float speed = 3.5f;
         float delta = Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            heliSprite.translateX(speed*delta);
+            movingRight = true;
+            heliSprite.setFlip(true, false);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            heliSprite.translateX(-speed*delta);
+            heliSprite.setFlip(false, false);
+            movingRight = false;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            heliSprite.translateY(speed*delta*0.75f);
+            movingUp = true;
+        }else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            heliSprite.translateY(-speed*delta*1.25f);
+            movingUp = false;
+        }
+    }
+
+    private void logic(){
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
         float heliWidth = heliSprite.getWidth();
         float heliHeight = heliSprite.getHeight();
 
+        if(!isControlled()){
+            float delta = Gdx.graphics.getDeltaTime();
 
-        if(heliSprite.getX() > worldWidth - heliWidth){
-            movingRight = false;
-            currentXSpeed = setRandomXSpeed();
-        }else if(heliSprite.getX() < 0){
-            movingRight = true;
-            currentXSpeed = setRandomXSpeed();
+
+            if(heliSprite.getX() >= worldWidth - heliWidth){
+                movingRight = false;
+                currentXSpeed = setRandomXSpeed();
+            }else if(heliSprite.getX() <= 0){
+                movingRight = true;
+                currentXSpeed = setRandomXSpeed();
+            }
+
+            if(heliSprite.getY() >= worldHeight - heliHeight){
+                movingUp = false;
+                currentYSpeedDown = setRandomYSpeedDown();
+            } else if (heliSprite.getY() <= 0) {
+                movingUp = true;
+                currentYSpeedUp = setRandomYSpeedUp();
+            }
+
+            if(movingRight){
+                heliSprite.translateX(currentXSpeed*delta);
+                heliSprite.setFlip(true, false);
+            }else {
+                heliSprite.translateX(-currentXSpeed*delta);
+                heliSprite.setFlip(false, false);
+            }
+
+            if(movingUp){
+                heliSprite.translateY(currentYSpeedUp*delta);
+            } else {
+                heliSprite.translateY(-currentYSpeedDown*delta);
+            }
         }
 
-        if(heliSprite.getY() > worldHeight - heliHeight){
-            movingUp = false;
-            currentYSpeedDown = setRandomYSpeedDown();
-        } else if (heliSprite.getY() < 0) {
-            movingUp = true;
-            currentYSpeedUp = setRandomYSpeedUp();
-        }
+        heliSprite.setX(MathUtils.clamp(heliSprite.getX(), 0, worldWidth - heliWidth));
+        heliSprite.setY(MathUtils.clamp(heliSprite.getY(), 0, worldHeight - heliHeight));
 
-        if(movingRight){
-            heliSprite.translateX(currentXSpeed*delta);
-            heliSprite.setFlip(true, false);
-        }else {
-            heliSprite.translateX(-currentXSpeed*delta);
-            heliSprite.setFlip(false, false);
-        }
-
-        if(movingUp){
-            heliSprite.translateY(currentYSpeedUp*delta);
-        } else {
-            heliSprite.translateY(-currentYSpeedDown*delta);
-        }
 
     }
 
